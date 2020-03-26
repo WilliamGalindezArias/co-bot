7995b624dbf5b7eec48b397cdda00ae242323356
William Galindez Arias <william.dev.env@gmail.com>
2020-03-26 19:21:45 +0100

    Update rasa_services.sh

--- rasa_services.sh
+++ rasa_services.sh
@@ -1,1 +1,2 @@
-rasa run actions --actions actions& rasa run --p $PORT --debug --cors "*" --enable-api --endpoints endpoints.yml --log-file out.log
+#rasa run actions --actions actions& rasa run --p $PORT --debug --cors "*" --enable-api --endpoints endpoints.yml --log-file out.log
+rasa run --p $PORT --debug --cors "*" --enable-api --endpoints endpoints.yml --log-file out.log
